884b452f6e8c427997a8f8bec5fecdb04337397c
Igor Polyakov <igor_polyakov@phystech.edu>
2016-12-11 23:59:11 +0300

    all examples have been explored

--- Код/Web/JavaScript/three.js
+++ Код/Web/JavaScript/three.js
@@ -29,12 +29,18 @@ var renderer = window.WebGLRenderingContext ? new THREE.WebGLRenderer() : new TH
 
 
 // Инструменты
-// [Онлайн редактор](https://threejs.org/editor/)
+// [Текстовый онлайн редактор](https://threejs.org/editor/)
+// [Интерактивный текстовый онлайн редактор](https://brian.peiris.io/RiftSketch/)
+// [Текстовый CoffeeScript онлайн редактор](http://www.sketchpatch.net/labs/livecodelabIntro.html)
+// [Логический онлайн редактор](http://idflood.github.io/ThreeNodes.js/)
+// [Графический онлайн редактор](http://blackjk3.github.io/threefab/)
+// https://aframe.io/
+// https://clara.io/
+// http://vr.chromeexperiments.com/
+// http://www.shapespark.com/
+// https://sparklinlabs.itch.io/superpowers
 
 // Примеры
-// https://stemkoski.github.io/Three.js/
-// https://github.com/stemkoski/three.js
-// https://github.com/mrdoob/three.js
 // http://www.vill.ee/eye/
 // https://threejs.org/examples/#webgl_geometry_colors_lookuptable
 // https://threejs.org/examples/#webgl_multiple_elements_text
@@ -45,6 +51,29 @@ var renderer = window.WebGLRenderingContext ? new THREE.WebGLRenderer() : new TH
 // https://threejs.org/examples/#webgl_loader_mmd
 // https://threejs.org/examples/#webgl_loader_ctm_materials
 // https://threejs.org/examples/#webgl_loader_collada_keyframe
+// http://www.playkeepout.com/
+// http://networkeffect.io/
+// http://www.porsche.com/microsite/911/usa.aspx#showroom/911-carrera-s/3/3/50/0/0
+// http://campaign.au.kddi.com/hello/
+// http://oletus.github.io/lasertown/
+// https://virtualart.chromeexperiments.com/
+// https://predictiveworld.watchdogs.com/en/
+
+// Коллекции примеров
+// https://threejs.org/
+// https://threejs.org/examples
+// http://mrdoob.com/
+// https://www.chromeexperiments.com/webgl
+// https://www.chromeexperiments.com/globe
+// http://helloenjoy.com/
+
+// Интерактивные музыкальные клипы
+// http://www.ro.me/
+// http://acko.net/
+// http://lights.helloenjoy.com/
+// https://www.justareflektor.com/
+// https://throughthedark.withgoogle.com/
+
 
 // Источники
 // [ ] https://www.pluralsight.com/courses/webgl-threejs-fundamentals
@@ -55,6 +84,8 @@ var renderer = window.WebGLRenderingContext ? new THREE.WebGLRenderer() : new TH
 // [ ] https://code.tutsplus.com/tutorials/webgl-with-threejs-basics--net-35688
 // [ ] https://habrahabr.ru/post/224509/
 // https://pragprog.com/book/csjava/3d-game-programming-for-kids
+// http://tympanus.net/codrops/2016/04/26/the-aviator-animating-basic-3d-scene-threejs/
+// https://stemkoski.github.io/Three.js/
 
 // Альтернативные библиотеки для WebGL
 // [Babylon.js](babylonjs.com)
