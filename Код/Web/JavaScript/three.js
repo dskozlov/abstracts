29e6160361d71982a88405ad6a8a285249024013
iGor <igor_polyakov@phystech.edu>
2016-12-12 17:16:07 +0300

    working with models

--- Код/Web/JavaScript/three.js
+++ Код/Web/JavaScript/three.js
@@ -18,6 +18,12 @@
 // - освещение
 // - актёры и декорации
 
+// Причины не увидеть объект:
+// - ошибка в коде
+// - перекрытие объектов
+// - отсутствие света
+// - маленький масштаб
+
 
 // Сцена
 var scene = new THREE.Scene();
@@ -48,15 +54,10 @@ var camera = new THREE.PerspectiveCamera(FOV, aspect_ratio, near_plane, far_plan
 // near_plane [int] — расстояние до плоскости, ближе которой объекты не будут видны
 // far_plane [int] — расстояние до плоскости, дальше которой объекты не будут видны
 
-// Расположение камеры
-camera.position.x = 10;
-camera.position.y = 10;
-camera.position.z = 10;
 // Добавление на сцену
 scene.add(camera);
 
 
-// Примитивы
 // Общая методика создания объекта
 // Создаём геометрическую модель объекта
 var geometry = new THREE.BoxGeometry(1, 1, 1);
@@ -66,6 +67,95 @@ var material = new THREE.MeshBasicMaterial({color: 0xff0000});
 var box = THREE.Mesh(geometry, material);
 // Добавляем на сцену
 scene.add(box);
+
+
+// Геометрические модели
+// Встроенные модели (примитивы)
+new THREE.BoxGeometry(width, height, depth);
+new THREE.SphereGeometry(radius, widthSegments, heightSegments);  // сфера
+
+// Произвольные модели
+// готовим объект для модели
+var myGeometry = new THREE.Geometry();
+// добавляем вершины модели
+myGeometry.vertices.push(new THREE.Vector3(0, 1, 0));
+myGeometry.vertices.push(new THREE.Vector3(-1, -1, 0));
+myGeometry.vertices.push(new THREE.Vector3(1, -1, 0));
+// образуем грань из точек
+// Дополнительно можно задать цвет вершинам (тогда грань раскрасится по градиенту)
+myGeometry.faces[0].vertexColors[0] = new THREE.Color(0xff0000);
+myGeometry.faces[0].vertexColors[1] = new THREE.Color(0x00ff00);
+myGeometry.faces[0].vertexColors[2] = new THREE.Color(0x0000ff);
+// делаем Mesh
+myMesh = new THREE.Mesh(myGeometry);
+
+// Импортированные модели
+// https://github.com/mrdoob/three.js/tree/dev/src/loaders
+// Для загрузки каждого из форматов (допустимы практические любые известные) требуется специальный загрузчик (loader), который можно взять из проекта three.js
+// Стандартный (родной) формат — three.js
+var loader = THREE.JSONLoader();
+loader.load("./obj.js", function (geometry) {
+  obj = new THREE.Mesh(geometry);
+  scene.add(obj);
+});
+
+// Экспортирование модели
+// производится с помощью вспомогательных библиотек GeometryExporter.js или SceneExporter.js
+
+
+// Материалы
+// если материал не указывается, то объекту присваивается произвольный цвет
+.MeshBasicMaterial({ // не взаимодействует со светом
+  vertexColors: THREE.VertexColors,
+  side: THREE.DoubleSide
+});
+
+// Примитивы, камеры, свет являются объектами класса Object3D.
+// Для объектов Object3D характерны следующие свойства
+.id
+.uuid
+.name
+.position
+.userData
+.parent
+.children
+// и методы
+.getObjectByName()
+.getObjectById()
+.lookAt()
+
+// Трансформации
+.position // перемещение (в координатах)
+.scale    // масштабирование (в коэффициентах)
+.rotate   // вращение (вокруг соответствующей оси в радианах = градусы * pi / 180)
+// все трансформации родителей также распространяются и на детей
+
+// Трансформации могут задаваться тремя различными способами
+obj.position.x = 10;
+obj.position.set(x, y, z);
+obj.position = new THREE.Vector3(x, y, z);
+
+// Модификации моделей
+// изменение положения вершины
+obj.geometry.vertices[0].z = 1;
+// Можно также вычитать модели друг из друга, складывать и оставлять пересечённую часть
+// http://evanw.github.io/csg.js/
+
+
+// Вспомогательные элементы
+// Отображение статистики
+// https://github.com/mrdoob/stats.js
+var stats = new Stats();
+stats.setMode(0);
+stats.domElement.style({
+  position: "absolute",
+  left: 0,
+  top: 0
+});
+document.getElementById("tag_id").appendChild(stats.domElement);
+// обновление статистики
+stats.update();
+
 
 
 // Инструменты
@@ -114,18 +204,27 @@ scene.add(box);
 // https://www.justareflektor.com/
 // https://throughthedark.withgoogle.com/
 
-
 // Источники
+// [ ] https://threejs.org/docs/
+// [ ] http://davidscottlyons.com/threejs/presentations/frontporch14/#slide-0
 // [ ] https://www.pluralsight.com/courses/webgl-threejs-fundamentals
+// [ ] Jos Dirksen - Three.js Essentials - 2014
+// [ ] Dirksen J. - Learning Three.js The JavaScript 3D Library for WebGL (2nd Edition) - 2015
+// [ ] Frahaan Hussain - Three js and WebGL 3D Programming
+// [ ] https://www.udemy.com/threejs-programming/
+// [ ] Jos Dirksen - Three.js Cookbook - 2015
+// [ ] Isaac Sukin - Game Development with Three.js - 2013
 // [ ] https://aerotwist.com/tutorials/getting-started-with-three-js/
 // [ ] http://www.smartjava.org/content/all-109-examples-my-book-threejs-threejs-version-r63
-// [ ] https://threejs.org/docs/
 // [ ] http://learningwebgl.com/blog/?page_id=1217
 // [ ] https://code.tutsplus.com/tutorials/webgl-with-threejs-basics--net-35688
 // [ ] https://habrahabr.ru/post/224509/
-// https://pragprog.com/book/csjava/3d-game-programming-for-kids
-// http://tympanus.net/codrops/2016/04/26/the-aviator-animating-basic-3d-scene-threejs/
-// https://stemkoski.github.io/Three.js/
+// [ ] https://pragprog.com/book/csjava/3d-game-programming-for-kids
+// [ ] http://tympanus.net/codrops/2016/04/26/the-aviator-animating-basic-3d-scene-threejs/
+// [ ] https://stemkoski.github.io/Three.js/
+
+// Стеки моделей
+// http://tf3dm.com/
 
 // Альтернативные библиотеки для WebGL
 // [Babylon.js](babylonjs.com)
