8a011b27b4cb091854d06f23d3fb95d6963c7bbc
Igor Polyakov <igor_polyakov@phystech.edu>
2017-06-19 02:19:38 +0300

    sql basics added

--- Код/SQL.sql
+++ Код/SQL.sql
@@ -1,2 +1,13 @@
+-- SQL
 
+-- выбрать данные из таблицы
+SELECT col1, col2 FROM table; -- выбрать конкретные колонки из таблицы
+SELECT * FROM table; -- выбрать все колонки
+SELECT col1 FROM table WHERE id = 1; -- конкретизировать выбор строк (можно также пользоваться знаками >, <, >=, <=, != (или <>) )
+SELECT col1 FROM table WHERE name = 'iGor' OR name = 'Alenushka' AND surname = 'Polyakov'; -- более сложная выборка
+SELECT * FROM table ORDER BY col1; -- выбрать в порядке возрастания
+SELECT * FROM table ORDER BY col1 DESC; -- выбрать в порядке убывания
 
+-- вставить данные в таблицу
+INSERT INTO table (col1, col2)
+VALUES ('Something', 42)
