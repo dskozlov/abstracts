55ea9ebe29fdb3b63864780dacdf68c8b22781dc
iGor Polyakov <igor_polyakov@phystech.edu>
2017-06-18 19:30:36 +0300

    sails added

---
+++ Код/Web/JavaScript/sails.js
@@ -0,0 +1,7 @@
+// sails.js
+
+
+// Источники
+// Сайт http://sailsjs.com
+// Официальная документация http://sailsjs.com/documentation/concepts
+// Быстрое введение в курс дела https://www.youtube.com/playlist?list=PLF8BRFFRWv9VkafkA4JtgY0I5NIzX2ZI9
